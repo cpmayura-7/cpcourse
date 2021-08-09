171a9cf62c2f2b0752e97e851defea146dfc9d9c
cpmayura-7 <cpmayura-7>
2021-08-09 18:57:45 +0530

    mod2

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,12 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	if(n==0):
+		return True
+	if(f==0):
+		return False
+	if(n%f==0):
+		return True
+	else:
+		return False
+	
