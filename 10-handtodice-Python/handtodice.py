627c19266dc2468e470401ad3f317a701817729f
cpmayura-7 <cpmayura-7>
2021-08-09 12:43:06 +0530

    mod1

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -6,5 +6,10 @@
 # Hint: You might find // and % useful here, and also getKthDigit().
 
 def handtodice(hand):
-	# your code goes here
-	pass
+	st=[]
+	while(hand!=0):
+		x=hand%10
+		hand=hand//10
+		st.append(x)
+	st=st[::-1]
+	return tuple(st)
